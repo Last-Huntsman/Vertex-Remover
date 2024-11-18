4b1a99de8fdeb012d55dc34407bb09d538d73a6b
Egor <zuzik17@yandex.ru>
2024-11-18 23:11:18 +0300

    Отдельно хранятся индексы из модели для всех типов, которые надо будет проверить и удалить, в полигонах по прежнему удаляется

--- ObjReaderInitial/src/com/cgvsu/VertexDelete/Eraser.java
+++ ObjReaderInitial/src/com/cgvsu/VertexDelete/Eraser.java
@@ -3,9 +3,9 @@ package com.cgvsu.VertexDelete;
 import com.cgvsu.model.Model;
 import com.cgvsu.model.Polygon;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Eraser {
     public static Model vertexDelete(Model model, List<Integer> index, boolean new_file, boolean hanging_peaks) {
@@ -17,31 +17,29 @@ public class Eraser {
         Collections.sort(index);
         Collections.reverse(index);
 
-        ArrayList<Integer> deletetextureVertices = new ArrayList<>();
-        ArrayList<Integer> deletenormals = new ArrayList<>();
-
+        TreeSet<Integer> deletetextureVertices = new TreeSet<Integer>();
+        TreeSet<Integer> deletenormals = new TreeSet<Integer>();
+        TreeSet<Integer> deletevertices = new TreeSet<Integer>();
 
         for (int i = 0; i < model.polygons.size(); i++) {
             Polygon polygon = model.polygons.get(i);
             for (int q = 0; q < index.size(); q++) {
                 for (int j = 0; j < polygon.getVertexIndices().size(); j++) {
                     if (polygon.getVertexIndices().get(j).equals(index.get(q))) {
-                        model.vertices.remove(index.get(q));
+                        deletetextureVertices.add(polygon.getVertexIndices().get(j));
                         deletenormals.add(polygon.getNormalIndices().get(j));
                         deletetextureVertices.add(polygon.getTextureVertexIndices().get(j));
 
                         polygon.getVertexIndices().remove(j);
                         polygon.getNormalIndices().remove(j);
                         polygon.getTextureVertexIndices().remove(j);
-
                     }
-
-
                 }
-                model.vertices.remove((Integer) index.get(q));
+
 
             }
         }
 
+        return model;
     }
 }
