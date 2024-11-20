7e7ff708d001a3f4b180039cceadb0fa2b3c24c3
Egor <zuzik17@yandex.ru>
2024-11-20 17:43:18 +0300

    В основном методе происходит перерасчет полигонов модели после чего в методах уже происходит работа с оставшимеся списками вершин. ПРОБЛЕМА!!! непонятно как пересчитать индексы

--- ObjReaderInitial/src/com/cgvsu/VertexDelete/Eraser.java
+++ ObjReaderInitial/src/com/cgvsu/VertexDelete/Eraser.java
@@ -9,64 +9,51 @@ public class Eraser {
 
     private static Set<Integer> deletetextureVertices = new HashSet<Integer>();
     private static Set<Integer> deletenormals = new HashSet<Integer>();
-    private static Set<Integer> deletevertices = new HashSet<Integer>();
 
 
     public static Model vertexDelete(Model model, List<Integer> index, boolean new_file, boolean hanging_peaks) {
 
-        Model rezultModel;
-        if (new_file) rezultModel = model.clone();
-        else rezultModel = model;
-
+        ArrayList<Polygon> polygons = new ArrayList<>();
         Collections.sort(index);
         Collections.reverse(index);
-
-        Set<Integer> deletetextureVertices = new HashSet<Integer>();
-        Set<Integer> deletenormals = new HashSet<Integer>();
-        Set<Integer> deletevertices = new HashSet<Integer>();
-
-
-        if (new_file) return vertexDeletenewF(model, index, hanging_peaks);
-
-        else return vertexDeleteoldF(model, index, hanging_peaks);
-    }
-
-    public static Model vertexDeletenewF(Model model, List<Integer> index, boolean hanging_peaks) {
-        Model modelrez = new Model();
-
 
         for (int i = 0; i < model.polygons.size(); i++) {
             Polygon polygon = model.polygons.get(i);
             Polygon polygonrez = new Polygon();
 
-            ArrayList<Integer> vertexIndices =new ArrayList<>();
-            ArrayList<Integer> textureVertexIndices =new ArrayList<>();
-            ArrayList<Integer> normalIndices =new ArrayList<>();
+            Set<Integer> textureVertexIndices = new HashSet<Integer>();
+            Set<Integer> normalIndices = new HashSet<Integer>();
+            Set<Integer> vertexIndices = new HashSet<Integer>();
 
             for (int j = 0; j < polygon.getVertexIndices().size(); j++) {
                 if (index.contains(polygon.getVertexIndices().get(j))) {
-                    deletevertices.add(polygon.getVertexIndices().get(j));
                     deletenormals.add(polygon.getNormalIndices().get(j));
                     deletetextureVertices.add(polygon.getTextureVertexIndices().get(j));
-
-                    polygon.getVertexIndices().remove((int) j);
-                    polygon.getNormalIndices().remove((int) j);
-                    polygon.getTextureVertexIndices().remove((int) j);
-
-                    if (polygon.getVertexIndices().size() < 3) {
-                        vertexIndices.add(polygon.getVertexIndices().get(j));
-                        textureVertexIndices.add(polygon.getTextureVertexIndices().get(j));
-                        i--;
-                    }
                 } else {
-
-
+                    vertexIndices.add(polygon.getVertexIndices().get(j));
+                    textureVertexIndices.add(polygon.getTextureVertexIndices().get(j));
+                    normalIndices.add(polygon.getNormalIndices().get(j));
                 }
             }
+            polygonrez.setNormalIndices(new ArrayList<>(normalIndices));
+            polygonrez.setTextureVertexIndices(new ArrayList<>(textureVertexIndices));
+            polygonrez.setVertexIndices(new ArrayList<>(vertexIndices));
+
+            if (polygonrez.getNormalIndices().size() > 2) polygons.add(polygonrez);
+
         }
+        model.polygons=polygons;
+
+        if (new_file) return vertexDeletenewF(model, polygons, index, hanging_peaks);
+        else return vertexDeleteoldF(model, index, hanging_peaks);
+    }
+
+    private static Model vertexDeletenewF(Model model, ArrayList<Polygon> polygons, List<Integer> index, boolean hanging_peaks) {
+        Model modelrez = new Model();
+        modelrez.polygons=polygons;
 
         for (int i = 0; i < model.vertices.size(); i++) {
-            if (!deletevertices.contains(i)) {
+            if (!index.contains(i)) {
                 modelrez.vertices.add(model.vertices.get(i).clone());
             }
         }
@@ -76,55 +63,30 @@ public class Eraser {
             }
         }
 
-        for (Integer integer : normalsList) {
-            boolean f = true;
-            pol:
-            for (int i = 0; i < rezultModel.polygons.size(); i++) {
-                Polygon polygon = rezultModel.polygons.get(i);
+        for (int i = 0; i < model.normals.size(); i++) {
+            for (int j = 0; j < modelrez.polygons.size(); j++) {
+                Polygon polygon = modelrez.polygons.get(j);
                 for (int k = 0; k < polygon.getNormalIndices().size(); k++) {
-                    if (integer.equals(polygon.getNormalIndices().get(k))) {
-                        f = false;
-                        break pol;
+                    if (polygon.getNormalIndices().get(k).equals(i)) {
+                        modelrez.normals.add(model.normals.get(i).clone());
                     }
                 }
+
             }
-            if (f) rezultModel.normals.remove((int) integer);
         }
 
 
+        return modelrez;
     }
 
-    public static Model vertexDeleteoldF(Model model, List<Integer> index, boolean hanging_peaks) {
-
-        for (int i = 0; i < model.polygons.size(); i++) {
-            Polygon polygon = model.polygons.get(i);
-            for (int q = 0; q < index.size(); q++) {
-                for (int j = 0; j < polygon.getVertexIndices().size(); j++) {
-                    if (polygon.getVertexIndices().get(j).equals(index.get(q))) {
-                        deletevertices.add(polygon.getVertexIndices().get(j));
-                        deletenormals.add(polygon.getNormalIndices().get(j));
-                        deletetextureVertices.add(polygon.getTextureVertexIndices().get(j));
-
-                        polygon.getVertexIndices().remove((int) j);
-                        polygon.getNormalIndices().remove((int) j);
-                        polygon.getTextureVertexIndices().remove((int) j);
-
-                        if (polygon.getVertexIndices().size() < 3) {
-                            model.polygons.remove(polygon);
-                            i--;
-                        }
-                    }
-                }
-            }
-        }
+    private static Model vertexDeleteoldF(Model model, List<Integer> index, boolean hanging_peaks) {
 
         List<Integer> textureVerticesList = new ArrayList<>(deletetextureVertices);
         List<Integer> normalsList = new ArrayList<>(deletenormals);
-        List<Integer> verticesList = new ArrayList<>(deletevertices);
 
 
-        for (int i = verticesList.size() - 1; i >= 0; i--) {
-            model.vertices.remove((int) verticesList.get(i));
+        for (int i = index.size() - 1; i >= 0; i--) {
+            model.vertices.remove((int) index.get(i));
         }
         for (int i = textureVerticesList.size() - 1; i >= 0; i--) {
             model.textureVertices.remove((int) textureVerticesList.get(i));
