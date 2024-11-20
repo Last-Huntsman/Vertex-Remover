016b266ec63aa3edc0f404c3f387604c53dc7f6f
Egor <zuzik17@yandex.ru>
2024-11-20 20:12:13 +0300

    Один метод под все, удаление вершин происоходит через перебор и переписывание валидных данных в новый файл

--- ObjReaderInitial/src/com/cgvsu/VertexDelete/Eraser.java
+++ ObjReaderInitial/src/com/cgvsu/VertexDelete/Eraser.java
@@ -12,10 +12,16 @@ public class Eraser {
 
 
     public static Model vertexDelete(Model model, List<Integer> index, boolean new_file, boolean hanging_peaks) {
-
+        Model modelrez = new Model();
         ArrayList<Polygon> polygons = new ArrayList<>();
         Collections.sort(index);
         Collections.reverse(index);
+
+        Map<Integer, Integer> connectionVertexIndices = new HashMap<>();
+
+        Map<Integer, Integer> connectionNormalIndices = new HashMap<>();
+
+        Map<Integer, Integer> connectiontextureVertexIndices = new HashMap<>();
 
         for (int i = 0; i < model.polygons.size(); i++) {
             Polygon polygon = model.polygons.get(i);
@@ -24,90 +30,52 @@ public class Eraser {
             Set<Integer> textureVertexIndices = new HashSet<Integer>();
             Set<Integer> normalIndices = new HashSet<Integer>();
             Set<Integer> vertexIndices = new HashSet<Integer>();
-
+            int k = 0;
             for (int j = 0; j < polygon.getVertexIndices().size(); j++) {
-                if (index.contains(polygon.getVertexIndices().get(j))) {
-                    deletenormals.add(polygon.getNormalIndices().get(j));
-                    deletetextureVertices.add(polygon.getTextureVertexIndices().get(j));
-                } else {
-                    vertexIndices.add(polygon.getVertexIndices().get(j));
-                    textureVertexIndices.add(polygon.getTextureVertexIndices().get(j));
-                    normalIndices.add(polygon.getNormalIndices().get(j));
+                if (!index.contains(polygon.getVertexIndices().get(j))) k++;
+            }
+            if (k < 3) continue;
+            for (int j = 0; j < polygon.getVertexIndices().size(); j++) {
+                if (index.contains(polygon.getVertexIndices().get(j))) continue;
+                else {
+                    if (!connectionVertexIndices.containsKey(polygon.getVertexIndices().get(j))) {
+                        vertexIndices.add(modelrez.vertices.size());
+                        connectionVertexIndices.put(polygon.getVertexIndices().get(j), modelrez.vertices.size());
+                        modelrez.vertices.add(new_file ? model.vertices.get(polygon.getVertexIndices().get(j)).clone() : model.vertices.get(polygon.getVertexIndices().get(j)));
+                    } else {
+                        vertexIndices.add(connectionVertexIndices.get(polygon.getVertexIndices().get(j)));
+                    }
+
+                    if (!connectiontextureVertexIndices.containsKey(polygon.getTextureVertexIndices().get(j))) {
+                        textureVertexIndices.add(modelrez.textureVertices.size());
+                        connectiontextureVertexIndices.put(polygon.getTextureVertexIndices().get(j), modelrez.textureVertices.size());
+                        modelrez.textureVertices.add(new_file ? model.textureVertices.get(polygon.getTextureVertexIndices().get(j)).clone() : model.textureVertices.get(polygon.getTextureVertexIndices().get(j)));
+                    } else {
+                        textureVertexIndices.add(connectiontextureVertexIndices.get(polygon.getTextureVertexIndices().get(j)));
+                    }
+
+                    if (!connectionNormalIndices.containsKey(polygon.getNormalIndices().get(j))) {
+                        normalIndices.add(modelrez.normals.size());
+                        connectionNormalIndices.put(polygon.getNormalIndices().get(j), modelrez.normals.size());
+                        modelrez.normals.add(new_file ? model.normals.get(polygon.getNormalIndices().get(j)).clone() : model.normals.get(polygon.getNormalIndices().get(j)));
+                    } else {
+                        normalIndices.add(connectionNormalIndices.get(polygon.getNormalIndices().get(j)));
+                    }
                 }
             }
+
             polygonrez.setNormalIndices(new ArrayList<>(normalIndices));
             polygonrez.setTextureVertexIndices(new ArrayList<>(textureVertexIndices));
             polygonrez.setVertexIndices(new ArrayList<>(vertexIndices));
 
-            if (polygonrez.getNormalIndices().size() > 2) polygons.add(polygonrez);
 
-        }
-        model.polygons=polygons;
-
-        if (new_file) return vertexDeletenewF(model, polygons, index, hanging_peaks);
-        else return vertexDeleteoldF(model, index, hanging_peaks);
-    }
-
-    private static Model vertexDeletenewF(Model model, ArrayList<Polygon> polygons, List<Integer> index, boolean hanging_peaks) {
-        Model modelrez = new Model();
-        modelrez.polygons=polygons;
-
-        for (int i = 0; i < model.vertices.size(); i++) {
-            if (!index.contains(i)) {
-                modelrez.vertices.add(model.vertices.get(i).clone());
-            }
-        }
-        for (int i = 0; i < model.textureVertices.size(); i++) {
-            if (!deletetextureVertices.contains(i)) {
-                modelrez.textureVertices.add(model.textureVertices.get(i).clone());
-            }
-        }
-
-        for (int i = 0; i < model.normals.size(); i++) {
-            for (int j = 0; j < modelrez.polygons.size(); j++) {
-                Polygon polygon = modelrez.polygons.get(j);
-                for (int k = 0; k < polygon.getNormalIndices().size(); k++) {
-                    if (polygon.getNormalIndices().get(k).equals(i)) {
-                        modelrez.normals.add(model.normals.get(i).clone());
-                    }
-                }
-
-            }
+            polygons.add(polygonrez);
         }
 
 
-        return modelrez;
-    }
+        modelrez.polygons = polygons;
 
-    private static Model vertexDeleteoldF(Model model, List<Integer> index, boolean hanging_peaks) {
-
-        List<Integer> textureVerticesList = new ArrayList<>(deletetextureVertices);
-        List<Integer> normalsList = new ArrayList<>(deletenormals);
-
-
-        for (int i = index.size() - 1; i >= 0; i--) {
-            model.vertices.remove((int) index.get(i));
-        }
-        for (int i = textureVerticesList.size() - 1; i >= 0; i--) {
-            model.textureVertices.remove((int) textureVerticesList.get(i));
-        }
-
-
-        for (Integer integer : normalsList) {
-            boolean f = true;
-            pol:
-            for (int i = 0; i < model.polygons.size(); i++) {
-                Polygon polygon = model.polygons.get(i);
-                for (int k = 0; k < polygon.getNormalIndices().size(); k++) {
-                    if (integer.equals(polygon.getNormalIndices().get(k))) {
-                        f = false;
-                        break pol;
-                    }
-                }
-            }
-            if (f) model.normals.remove((int) integer);
-        }
-
-        return model;
+        if(new_file) return modelrez;
+        else return model=modelrez;
     }
 }
